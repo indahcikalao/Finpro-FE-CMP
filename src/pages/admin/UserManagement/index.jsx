efd3814332a0899f2aa5b70477357186d17ad4ec
Rhenald <renaldkarel@gmail.com>
2023-06-15 14:27:10 +0700

    feat: - add edit user drawer form (activate & assign role) - change activate user func flow

--- src/pages/admin/UserManagement/index.jsx
+++ src/pages/admin/UserManagement/index.jsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import axios from 'axios';
 import DataTable from 'react-data-table-component';
-import { Typography, Button } from '@material-tailwind/react';
+import {
+	Typography,
+	Button,
+	Drawer,
+	IconButton,
+} from '@material-tailwind/react';
+import { XMarkIcon } from '@heroicons/react/24/outline';
 
 import { Badge } from '../../../Components/Atoms';
 
-const ActionsColumn = ({ id }) => {
+const ActionsColumn = ({ row, handleEditUser }) => {
 	const handleDeleteUser = async (id) => {
 		try {
 			const response = await axios.delete(
@@ -26,7 +32,7 @@ const ActionsColumn = ({ id }) => {
 		<div className='flex gap-2'>
 			<Typography
 				as='button'
-				href='#'
+				onClick={() => handleEditUser(row)}
 				variant='small'
 				color='blue'
 				className='font-medium'
@@ -35,7 +41,7 @@ const ActionsColumn = ({ id }) => {
 			</Typography>
 			<Typography
 				as='button'
-				onClick={() => handleDeleteUser(id)}
+				onClick={() => handleDeleteUser(row.id)}
 				variant='small'
 				color='red'
 				className='font-medium'
@@ -48,6 +54,22 @@ const ActionsColumn = ({ id }) => {
 
 const UserManagement = () => {
 	const [data, setData] = React.useState([]);
+	const [editUser, setEditUser] = React.useState({});
+	const drawerRef = React.useRef();
+
+	const [open, setOpen] = React.useState(false);
+
+	const handleEditUser = (row) => {
+		setOpen(true);
+		drawerRef.current.style.transform = 'none';
+		setEditUser(row);
+	};
+
+	const handleCloseEditUser = () => {
+		setOpen(false);
+		drawerRef.current.style.transform =
+			'translateX(-300px) translateY(0px) translateZ(0px)';
+	};
 
 	React.useEffect(() => {
 		const getUsers = async () => {
@@ -90,63 +112,142 @@ const UserManagement = () => {
 			);
 		} catch (error) {
 			console.log('Error:', error);
+		} finally {
+			handleCloseEditUser();
 		}
 	};
 
 	return (
-		<div className='my-4 space-y-4'>
-			<Typography
-				variant='h1'
-				className='font-bold capitalize text-lg md:text-2xl'
-			>
-				user management
-			</Typography>
-			<DataTable
-				columns={[
-					{
-						name: 'Fullname',
-						selector: (row) => row.fullname,
-						sortable: true,
-					},
-					{
-						name: 'Username',
-						selector: (row) => row.username,
-						sortable: true,
-					},
-					{
-						name: 'Email',
-						selector: (row) => row.email,
-						sortable: true,
-					},
-					{
-						name: 'Status',
-						selector: (row) => (row.is_active ? 'Active' : 'Inactive'),
-						cell: (row) =>
-							row.is_active ? (
-								<Badge type='success'>Active</Badge>
-							) : (
-								<button
-									className='hover:opacity-80 whitespace-nowrap'
-									onClick={() => handleActivateUser(row.id)}
-								>
-									<Badge type='danger'>Activate User</Badge>
-								</button>
+		<React.Fragment>
+			<div className='my-4 space-y-4'>
+				<Typography
+					variant='h1'
+					className='font-bold capitalize text-lg md:text-2xl'
+				>
+					user management
+				</Typography>
+				<DataTable
+					columns={[
+						{
+							name: 'Fullname',
+							selector: (row) => row.fullname,
+							sortable: true,
+						},
+						{
+							name: 'Username',
+							selector: (row) => row.username,
+							sortable: true,
+						},
+						{
+							name: 'Email',
+							selector: (row) => row.email,
+							sortable: true,
+						},
+						{
+							name: 'Status',
+							selector: (row) => row.is_active,
+							cell: (row) =>
+								row.is_active ? (
+									<Badge type='success'>Active</Badge>
+								) : (
+									<Badge type='danger'>Inactive</Badge>
+								),
+						},
+						{
+							name: 'Role',
+							selector: (row) => row.role,
+						},
+						{
+							name: 'Actions',
+							button: true,
+							cell: (row) => (
+								<ActionsColumn row={row} handleEditUser={handleEditUser} />
 							),
-					},
-					{
-						name: 'Role',
-						selector: (row) => row.role,
-					},
-					{
-						name: 'Actions',
-						button: true,
-						cell: (row) => <ActionsColumn id={row.id} />,
-					},
-				]}
-				data={data}
-				pagination
-			/>
-		</div>
+						},
+					]}
+					data={data}
+					pagination
+				/>
+			</div>
+			<Drawer
+				ref={drawerRef}
+				open={open}
+				onClose={handleCloseEditUser}
+				className='transition-all p-4'
+			>
+				<div className='mb-6 flex items-center justify-between'>
+					<Typography variant='h5' color='blue-gray'>
+						Edit User
+					</Typography>
+					<IconButton
+						variant='text'
+						color='blue-gray'
+						onClick={handleCloseEditUser}
+					>
+						<XMarkIcon strokeWidth={2} className='h-5 w-5' />
+					</IconButton>
+				</div>
+				<div className='space-y-4'>
+					<div className='form-group'>
+						<label
+							htmlFor='fullname'
+							className='block mb-2 text-sm font-medium text-gray-900'
+						>
+							Fullname
+						</label>
+						<input
+							type='text'
+							name='fullname'
+							className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 disabled:opacity-50 disabled:cursor-not-allowed'
+							value={editUser.fullname}
+							disabled
+						/>
+					</div>
+					<div className='form-group'>
+						<label
+							htmlFor='email'
+							className='block mb-2 text-sm font-medium text-gray-900'
+						>
+							Email
+						</label>
+						<input
+							type='email'
+							name='email'
+							className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5 disabled:opacity-50 disabled:cursor-not-allowed'
+							value={editUser.email}
+							disabled
+						/>
+					</div>
+					<div className='form-group'>
+						<label
+							htmlFor='role'
+							className='block mb-2 text-sm font-medium text-gray-900'
+						>
+							Role
+						</label>
+						<select
+							name='role'
+							className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg block w-full p-2.5'
+							defaultValue=''
+							required
+							onChange={(e) =>
+								setEditUser({ ...editUser, role: e.target.value })
+							}
+							value={editUser.role}
+						>
+							<option value=''>Assign role</option>
+							<option value='admin'>Admin</option>
+							<option value='user'>User</option>
+						</select>
+					</div>
+					<div className='form-group'>
+						<Button fullWidth onClick={() => handleActivateUser(editUser.id)}>
+							Activate User
+						</Button>
+					</div>
+				</div>
+			</Drawer>
+		</React.Fragment>
 	);
 };
 
