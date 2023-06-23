0c2f92752730626f742b3543fdd8ccb3d598bfca
Rhenald <renaldkarel@gmail.com>
2023-06-23 16:31:46 +0700

    integrate personalized navbar and adjust its responsiveness

--- src/Components/Mollecule/Navbar/index.jsx
+++ src/Components/Mollecule/Navbar/index.jsx
@@ -8,28 +8,28 @@ import {
   Avatar,
 } from "@material-tailwind/react";
 import { Bars3Icon } from "@heroicons/react/24/outline";
+import { useAuth } from '../../../hooks';
 
 const Navbar = ({ openSidebar, setOpenSidebar }) => {
+  const { auth } = useAuth();
+
   return (
     <MTNavbar className="mx-auto px-4 max-w-none py-2">
-      <div className="flex items-center justify-between text-blue-gray-900">
+      <div className="flex items-center gap-4 justify-between text-blue-gray-900">
         <Typography
           as="a"
           href="#"
           variant="h6"
-          className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+          className="mr-4 cursor-pointer py-1.5 lg:ml-2 hidden lg:inline"
         >
           Cash Management Platform
         </Typography>
-        <div className="relative flex w-full gap-2 md:w-max">
+        <div className="relative w-full gap-2 md:w-max hidden sm:flex">
           <Input
             type="search"
             color="white"
             label="Type here..."
             className="pr-20"
-            containerProps={{
-              className: "min-w-[288px]",
-            }}
           />
           <Button
             size="sm"
@@ -39,25 +39,27 @@ const Navbar = ({ openSidebar, setOpenSidebar }) => {
             Search
           </Button>
         </div>
-        <div className="flex gap-3">
+        <div className="flex gap-3 justify-between sm:justify-end w-full lg:w-auto">
           <Typography variant="paragraph" className="cursor-pointer py-1.5 lg:ml-2">
-            Hai, Admin
+            Hai, <span className="font-semibold capitalize">{auth.fullname}</span>!
           </Typography>
-          <Avatar
-            variant="circular"
-            size="sm"
-            alt="candice wu"
-            className="border border-blue-500 p-0.5"
-            src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
-          />
-          <IconButton
-            variant="text"
-            color="blue-gray"
-            className="xl:hidden"
-            onClick={() => setOpenSidebar(!openSidebar)}
-          >
-            <Bars3Icon className="h-6 w-6" strokeWidth={2} />
-          </IconButton>
+          <div>
+            <Avatar
+              variant="circular"
+              size="sm"
+              alt="candice wu"
+              className="border border-blue-500 p-0.5"
+              src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+            />
+            <IconButton
+              variant="text"
+              color="blue-gray"
+              className="xl:hidden"
+              onClick={() => setOpenSidebar(!openSidebar)}
+            >
+              <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+            </IconButton>
+          </div>
         </div>
       </div>
     </MTNavbar>
