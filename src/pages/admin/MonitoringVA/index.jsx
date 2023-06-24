df90a62738eb3375a7b564d6932c0cda252ae384
gedearya <igd.ad.pratama@gmail.com>
2023-06-24 12:02:41 +0700

    fixing name file download spreadsheet

--- src/pages/admin/MonitoringVA/index.jsx
+++ src/pages/admin/MonitoringVA/index.jsx
@@ -113,7 +113,7 @@ const MonitoringVA = () => {
     const worksheet = XLSX.utils.json_to_sheet(data);
     const workbook = XLSX.utils.book_new();
     XLSX.utils.book_append_sheet(workbook, worksheet, "Transactions");
-    XLSX.writeFile(workbook, "transactions.xlsx");
+    XLSX.writeFile(workbook, "transactionVA.xlsx");
   };
 
   return (
