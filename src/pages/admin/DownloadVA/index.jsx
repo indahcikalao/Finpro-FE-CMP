1ce4f2eb7675f164e427bbefef86b5faf8f7f933
unknown <indahcikalao@gmail.com>
2023-06-27 12:33:44 +0700

    create data table for preview transaction history data (still use dummy data)

--- src/pages/admin/DownloadVA/index.jsx
+++ src/pages/admin/DownloadVA/index.jsx
@@ -1,10 +1,12 @@
-import { Input, Typography, Radio, Button } from "@material-tailwind/react";
 import React, { useState } from "react";
-import Datepicker from "react-tailwindcss-datepicker";
+import { Input, Typography, Radio, Button } from "@material-tailwind/react";
 import { CheckCircleIcon } from "@heroicons/react/24/solid";
-import * as Yup from "yup";
 import { useFormik } from "formik";
+import Datepicker from "react-tailwindcss-datepicker";
 import dayjs from "dayjs";
+import DataTable from "react-data-table-component";
+import * as XLSX from "xlsx/xlsx.mjs";
+import * as Yup from "yup";
 
 export default function DownloadVA() {
   const [date, setDate] = useState({
@@ -14,7 +16,8 @@ export default function DownloadVA() {
 
   const initialValues = {
     giroNumber: "",
-    date: { startDate: date.startDate, endDate: date.endDate },
+    startDate: date.startDate,
+    endDate: date.endDate,
     accountType: "Giro Account",
   };
 
@@ -39,14 +42,127 @@ export default function DownloadVA() {
     formik.values.date.endDate = newDate.endDate;
     setDate(newDate);
   };
+
   const handleRadioButtonValue = (e) =>
     (formik.values.accountType = e.target.value);
 
-  const handleSearch = () => {
+  const handlePreview = () => {
     console.log(formik.values);
+    handleGetData();
   };
+  const [data, setData] = React.useState(null);
+
+  const handleGetData = () => {
+    const generateDummyData = (count) => {
+      const dummyData = [];
+
+      for (let i = 1; i <= count; i++) {
+        const dummyAccountNumber = generateRandomAccountNumber(10);
+        const dummyDate = generateRandomDate();
+        const dummyTime = generateRandomTime();
+
+        const dummyRow = {
+          nomor_rekening_giro: dummyAccountNumber,
+          currency: "IDR",
+          tanggal_transaksi: dummyDate,
+          jam: dummyTime,
+          remax: `Test ${i}`,
+          teller: "37401",
+          category: generateRandomCategory(),
+          credit: formatCurrency(generateRandomBalance()),
+        };
+
+        dummyData.push(dummyRow);
+      }
+
+      return dummyData;
+    };
+
+    const generateRandomCategory = () => {
+      const category = ["Debit", "Credit"];
+      const index = generateRandomNumber(0, 1);
+
+      return category[index];
+    };
+
+    const generateRandomAccountNumber = (length) => {
+      let result = "";
+      const characters = "0123456789";
+      const charactersLength = characters.length;
+
+      for (let i = 0; i < length; i++) {
+        result += characters.charAt(
+          Math.floor(Math.random() * charactersLength)
+        );
+      }
+
+      return result;
+    };
+
+    const generateRandomDate = () => {
+      const startDate = new Date(2023, 0, 1).getTime();
+      const endDate = new Date().getTime();
+      const randomDate = new Date(
+        startDate + Math.random() * (endDate - startDate)
+      );
+      return randomDate.toISOString().split("T")[0];
+    };
+    const generateRandomTime = () => {
+      const startDate = new Date(2023, 0, 1).getTime();
+      const endDate = new Date().getTime();
+      const randomDate = new Date(
+        startDate + Math.random() * (endDate - startDate)
+      );
+      return randomDate.toISOString().split("T")[1].split(".")[0];
+    };
+
+    const generateRandomBalance = () => {
+      return Math.floor(Math.random() * 10000000 + 1000000);
+    };
+
+    const generateRandomNumber = (min, max) => {
+      return Math.floor(Math.random() * (max - min + 1) + min);
+    };
+
+    const formatCurrency = (value) => {
+      return new Intl.NumberFormat("id-ID", {
+        style: "currency",
+        currency: "IDR",
+        minimumFractionDigits: 0,
+        maximumFractionDigits: 0,
+      }).format(value);
+    };
+
+    const getTransaction = async () => {
+      try {
+        const dummyData = generateDummyData(150);
+        setData(dummyData);
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    getTransaction();
+  };
+
   const handleDownload = () => {
-    console.log(formik.values);
+    const worksheet = XLSX.utils.json_to_sheet(data);
+    const workbook = XLSX.utils.book_new();
+    XLSX.utils.book_append_sheet(workbook, worksheet, "Transactions");
+    const excelBuffer = XLSX.write(workbook, {
+      type: "buffer",
+      bookType: "xlsx",
+    });
+    const blob = new Blob([excelBuffer], {
+      type: "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet",
+    });
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.setAttribute("download", "TransactionHistory.xlsx");
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
   };
 
   return (
@@ -69,7 +185,7 @@ export default function DownloadVA() {
               size="lg"
               name="giroNumber"
               placeholder="11 Digits Giro Number"
-              className=" focus:!border-black border bg-white shadow-md !border-gray-500 shadow-blue-gray-900/5 text-blue-gray-500"
+              className="focus:!border-black border shadow-sm !border-gray-500 shadow-blue-gray-900/5 "
               labelProps={{
                 className: "hidden",
               }}
@@ -142,14 +258,14 @@ export default function DownloadVA() {
         </div>
         <div className="flex justify-end gap-4 mb-6">
           <Button
-            onClick={handleSearch}
+            onClick={handlePreview}
             disabled={
               (!date.startDate && !date.endDate) ||
               !formik.touched.giroNumber ||
               formik.errors.giroNumber
             }
           >
-            Search
+            Preview
           </Button>
           <Button
             onClick={handleDownload}
@@ -163,7 +279,106 @@ export default function DownloadVA() {
           </Button>
         </div>
       </div>
-      <div></div>
+      {data && (
+        <div className="my-4 space-y-4 border-2 rounded-lg">
+          <DataTable
+            columns={[
+              {
+                name: <b>No</b>,
+                selector: (row, i) => i + 1,
+                sortable: true,
+                width: "50px",
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Account Number</b>,
+                selector: (row) => row.nomor_rekening_giro,
+                sortable: true,
+                center: true,
+                width: "150px",
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Currency</b>,
+                selector: (row) => row.currency,
+                sortable: true,
+                center: true,
+                width: "100px",
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Date</b>,
+                selector: (row) => row.tanggal_transaksi,
+                sortable: true,
+                center: true,
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Time</b>,
+                selector: (row) => row.jam,
+                sortable: true,
+                center: true,
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Remark</b>,
+                selector: (row) => row.remax,
+                sortable: true,
+                center: true,
+                width: "100px",
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Teller</b>,
+                selector: (row) => row.teller,
+                sortable: true,
+                center: true,
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Category</b>,
+                selector: (row) => row.category,
+                sortable: true,
+                center: true,
+
+                style: {
+                  borderRight: "1px solid #dee2e6",
+                },
+              },
+              {
+                name: <b>Amount</b>,
+                selector: (row) => row.credit,
+                sortable: true,
+                center: true,
+                width: "100px",
+              },
+              // {
+              //   name: <b>Nomor VA</b>,
+              //   selector: (row) => row.nomor_virtual_account,
+              //   sortable: true,
+              //   center: true,
+              //   width: "100px",
+              // },
+            ]}
+            data={data}
+            pagination
+          />
+        </div>
+      )}
     </div>
   );
 }
