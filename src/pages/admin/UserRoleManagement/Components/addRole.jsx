4df78bfc364b43fe49f5470dd9eebb18d5bc2d9d
Azis <mukhamadazistholib278@gmail.com>
2023-06-29 22:18:35 +0700

    update: add role function

--- src/pages/admin/UserRoleManagement/Components/addRole.jsx
+++ src/pages/admin/UserRoleManagement/Components/addRole.jsx
@@ -7,18 +7,30 @@ import {
   DialogFooter,
   Input,
   Checkbox,
+  Typography,
 } from "@material-tailwind/react";
 import { UserPlusIcon } from "@heroicons/react/24/solid";
-import DataTable from "react-data-table-component";
-import axios from "axios";
 import Swal from "sweetalert2";
+import api from "../../../../api/axios";
 
 const url = process.env.REACT_APP_BASE_URL;
 
+const role_table_head = ["Role name", "Monitoring VA", "Download VA"];
+
 const AddRoleManagement = () => {
-  const [data, setData] = useState([]);
   const [newRoleName, setNewRoleName] = useState("");
-  const [newRoleData, setNewRoleData] = useState([]);
+  const [newRoleData, setNewRoleData] = useState([
+    {
+      resource: "Monitoring",
+      can_read: false,
+      can_write: false,
+    },
+    {
+      resource: "Download",
+      can_read: false,
+      can_write: false,
+    },
+  ]);
   const [open, setOpen] = useState(false);
 
   const handleOpen = () => setOpen(true);
@@ -26,19 +38,37 @@ const AddRoleManagement = () => {
   const handleClose = () => {
     setOpen(false);
     setNewRoleName("");
-    setNewRoleData([]);
+    setNewRoleData([
+      {
+        resource: "Monitoring",
+        can_read: false,
+        can_write: false,
+      },
+      {
+        resource: "Download",
+        can_read: false,
+        can_write: false,
+      },
+    ]);
   };
 
-  const handleConfirm = async () => {
-    try {
-      const response = await axios.post(`${url}/admin/role`, {
-        name: newRoleName,
-        data: newRoleData,
-      });
+  const handleRoleNameChange = (e) => {
+    setNewRoleName(e.target.value);
+  };
 
-      // Lakukan tindakan setelah peran berhasil dibuat, misalnya memperbarui tampilan atau mengambil data terbaru
+  const addRole = async () => {
+    try {
+      const roleData = {
+        role: newRoleName,
+        data: newRoleData,
+      };
+
+      const response = await api.post(`${url}/admin/role`, roleData);
+
       console.log("Role created:", response.data);
-      Swal.fire("Success", "Role created successfully!", "success");
+      Swal.fire("Success", "Role created successfully!", "success").then(() => {
+        window.location.reload();
+      });
 
       handleClose();
     } catch (error) {
@@ -48,30 +78,23 @@ const AddRoleManagement = () => {
     }
   };
 
+  const renderCheckboxes = (index, label, checked, onChange) => (
+    <Checkbox
+      id={`ripple-on-${label.toLowerCase()}-${index}`}
+      label={label}
+      ripple={true}
+      checked={checked}
+      onChange={onChange}
+    />
+  );
 
-  const handleAddRow = () => {
-    setNewRoleData([...newRoleData, { resource: "", can_read: false, can_write: false }]);
-  };
-
-  const handleDeleteRow = (index) => {
-    const updatedData = [...newRoleData];
-    updatedData.splice(index, 1);
-    setNewRoleData(updatedData);
-  };
-
-  const handleRoleChange = (index, value) => {
-    const updatedData = [...newRoleData];
-    updatedData[index].resource = value;
-    setNewRoleData(updatedData);
-  };
-
-  const handleReadChange = (index, checked) => {
+  const handleCanReadChange = (index, checked) => {
     const updatedData = [...newRoleData];
     updatedData[index].can_read = checked;
     setNewRoleData(updatedData);
   };
 
-  const handleWriteChange = (index, checked) => {
+  const handleCanWriteChange = (index, checked) => {
     const updatedData = [...newRoleData];
     updatedData[index].can_write = checked;
     setNewRoleData(updatedData);
@@ -87,61 +110,86 @@ const AddRoleManagement = () => {
       >
         <UserPlusIcon strokeWidth={2} className="h-4 w-4" /> Add Role
       </Button>
-      <Dialog open={open} handler={handleClose}>
+      <Dialog open={open} handler={handleClose} size="lg">
         <DialogHeader>Add New Role</DialogHeader>
         <DialogBody>
-          <Input
-            label="Role Name"
-            value={newRoleName}
-            onChange={(e) => setNewRoleName(e.target.value)}
-          />
-          <DataTable
-            columns={[
-              {
-                name: "Role",
-                cell: (row) => (
-                  <Input className="px-3"
-                    value={row.role}
-                    onChange={(e) => handleRoleChange(row.index, e.target.value)}
+          <table className="w-full table-auto text-center">
+            <thead>
+              <tr>
+                {role_table_head.map((head) => (
+                  <th
+                    key={head}
+                    className="border-b border-blue-gray-100 bg-blue-gray-50 p-4"
+                  >
+                    <Typography
+                      variant="small"
+                      color="blue-gray"
+                      className="font-normal leading-none opacity-70"
+                    >
+                      {head}
+                    </Typography>
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className="p-4">
+                  <Input
+                    variant="outlined"
+                    label="Role Name"
+                    type="text"
+                    value={newRoleName}
+                    onChange={handleRoleNameChange}
+                    className="mb-4"
                   />
-                ),
-              },
-              {
-                name: "Read",
-                cell: (row) => (
-                  <Checkbox
-                    checked={row.can_read}
-                    onChange={(e) => handleReadChange(row.index, e.target.checked)}
-                  />
-                ),
-              },
-              {
-                name: "Write",
-                cell: (row) => (
-                  <Checkbox
-                    checked={row.can_write}
-                    onChange={(e) => handleWriteChange(row.index, e.target.checked)}
-                  />
-                ),
-              },
-              {
-                name: "Actions",
-                cell: (row) => (
-                  <Button variant="outlined" color="red" onClick={() => handleDeleteRow(row.index)}>
-                    Delete
-                  </Button>
-                ),
-              },
-            ]}
-            data={newRoleData}
-          />
-          <Button className="ml-4" onClick={handleAddRow}>Add Row</Button>
+                </td>
+                <td className="p-4">
+                  <div>
+                    {renderCheckboxes(
+                      0,
+                      "Read",
+                      newRoleData[0].can_read,
+                      (e) => handleCanReadChange(0, e.target.checked)
+                    )}
+                    {renderCheckboxes(
+                      0,
+                      "Write",
+                      newRoleData[0].can_write,
+                      (e) => handleCanWriteChange(0, e.target.checked)
+                    )}
+                  </div>
+                </td>
+                <td className="p-4">
+                  <div>
+                    {renderCheckboxes(
+                      1,
+                      "Read",
+                      newRoleData[1].can_read,
+                      (e) => handleCanReadChange(1, e.target.checked)
+                    )}
+                    {renderCheckboxes(
+                      1,
+                      "Write",
+                      newRoleData[1].can_write,
+                      (e) => handleCanWriteChange(1, e.target.checked)
+                    )}
+                  </div>
+                </td>
+              </tr>
+            </tbody>
+          </table>
         </DialogBody>
         <DialogFooter>
-          <Button variant="text" color="red" onClick={handleClose} className="mr-1">
+          <Button
+            variant="text"
+            color="red"
+            onClick={handleClose}
+            className="mr-1"
+          >
             <span>Cancel</span>
           </Button>
-          <Button variant="gradient" color="green" onClick={handleConfirm}>
+          <Button variant="gradient" color="blue" onClick={addRole}>
             <span>Confirm</span>
           </Button>
         </DialogFooter>
