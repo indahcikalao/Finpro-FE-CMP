a3535ec250ef307c367e96c7f46b67298f323e42
Rhenald <renaldkarel@gmail.com>
2023-07-12 13:54:29 +0700

    add index exporter on pages folder

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export * from './admin';
+
+export { default as Login } from './login';
+export { default as Register } from './register';
+export { default as Forbidden } from './Forbidden';
+export { default as ResetPassword } from './resetPassword';
