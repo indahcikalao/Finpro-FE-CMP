e31bb3381d80aedf74c85a18a4862a57e4761de8
Rhenald <renaldkarel@gmail.com>
2023-07-12 13:46:33 +0700

    add exports of other pages in the folder

--- src/pages/admin/index.js
+++ src/pages/admin/index.js
@@ -1,2 +1,5 @@
 export { default as UserManagement } from './UserManagement';
 export { default as UserRoleManagement } from './UserRoleManagement';
+export { default as DownloadVA } from './DownloadVA';
+export { default as MonitoringVA } from './MonitoringVA';
+export { default as Homepage } from './Homepage';
