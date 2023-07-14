b1205ac4442ce484c8a5cef7ee891fb5163f4971
Rhenald <renaldkarel@gmail.com>
2023-07-14 22:54:59 +0700

    fix: homepage `class` -> `className` error

--- src/pages/Protected/Homepage/index.jsx
+++ src/pages/Protected/Homepage/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Homepage() {
   return (
-    <div class="grid place-items-center h-screen font-bold text-3xl text-center">
+    <div className="grid place-items-center h-screen font-bold text-3xl text-center">
       Welcome to <br />
       TRB Web Application
     </div>
